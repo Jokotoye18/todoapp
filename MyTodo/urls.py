cf48b337d0835f1418ebf9e82cba18010c9add10
jokotoye18 <jokotoyeademola95@gmail.com>
2020-12-18 11:25:55 +0100

    fix: no such table: django_session

--- MyTodo/urls.py
+++ MyTodo/urls.py
@@ -7,7 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), 
-    path("todo/", include('todoapp.urls')),
+    path("", include('todoapp.urls')),
     path('', include('contact.urls'))
 ]
 
